e56039c2549dde5c8200612f225eb69a0fd1ce3f
Vlad <brvld@rambler.ru>
2023-05-01 00:03:21 +0300

    Task2 done

--- seminar6/task2.py
+++ seminar6/task2.py
@@ -1,6 +1,7 @@
 # Задача 32: Определить индексы элементов массива (списка), значения которых
 # принадлежат заданному диапазону (т.е. не меньше заданного минимума и не больше
 # заданного максимума)
+
 import random
 
 n_element =  int(input('Введите сколько элементов списке, n: '))
@@ -11,8 +12,6 @@ my_list = [random.randint(1,20) for _ in range(n_element)]
 print(f'Список: {my_list}')
 print(f'  Срез: {my_list[first_element:last_element+1]}') if last_element < n_element \
     else print(f'Номер последнего элемента лежит за пределами списка: {my_list[first_element:]}')
-
-
 
 
 
