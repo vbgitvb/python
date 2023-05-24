4cece4e96a1e1d0ac92da1795d88601113d826e6
Vlad <brvld@rambler.ru>
2023-05-24 22:06:52 +0300

    Seminar7 task1 done

--- seminar7/task1.py
+++ seminar7/task1.py
@@ -13,3 +13,23 @@
 
 # **Ввод:** пара-ра-рам рам-пам-папам па-ра-па-да    
 # **Вывод:** Парам пам-пам  
+
+def compare_count_syllable(text):
+    rus_vowels = "ауоыиэяюёе"
+    count_syllable = 0
+    first = True
+    count_first_syllable = 0
+    for word in phrase.split(" "):
+        count_syllable = len([item for item in word if item in rus_vowels])
+        if first: 
+            first = False
+            count_first_syllable = count_syllable
+        else:
+            if count_first_syllable != count_syllable:
+                return False
+    return True
+
+phrase = "пара-ра-рам рам-пам-папам па-ра-па-да"
+
+print(phrase)
+print("Парам пам-пам") if compare_count_syllable(phrase) else print("Пам парам")
